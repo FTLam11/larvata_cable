fb48b74dae859b9800c33bb68b6264ae9416cdc4
Frank Lam <ryzingsun11@yahoo.com>
2019-07-26 14:40:36 +0800

    Fix describe description for Chatter

--- spec/models/larvata_cable/chatter_spec.rb
+++ spec/models/larvata_cable/chatter_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 module LarvataCable
   RSpec.describe Chatter, type: :model do
-    describe 'validations' do
+    describe 'associations' do
       it { should belong_to(:user) }
       it { should belong_to(:chat_room) }
     end
