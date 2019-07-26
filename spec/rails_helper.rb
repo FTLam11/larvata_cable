d7db4eee79faf94ad2dee6cccea04632e3fb09a5
Frank Lam <ryzingsun11@yahoo.com>
2019-07-26 14:28:45 +0800

    Setup shoulda-matchers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,13 +5,13 @@ require File.expand_path('../dummy/config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'support/factory_bot'
-require 'support/request_helper'
+require 'shoulda-matchers'
 require 'rails-controller-testing'
 require 'awesome_print'
 require 'pry'
 require 'pry-byebug'
 require 'pry-rails'
+Dir['./spec/support/**/*.rb'].each {|f| require f}
 Rails::Controller::Testing.install
 # Add additional requires below this line. Rails is not loaded until this point!
 
