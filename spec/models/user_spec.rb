a1c86e289cd51215dafc8ab2f06ee75f4b2348e3
Frank Lam <ryzingsun11@yahoo.com>
2019-07-26 10:03:46 +0800

    Finish standin User spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'has an account' do
+  it 'has a unique account' do
     user = create(:user, account: 'Falcon')
+    duplicate_user = build(:user, account: 'Falcon')
 
     expect(user.account).to eq 'Falcon'
+    expect(duplicate_user).to be_invalid
   end
 end
