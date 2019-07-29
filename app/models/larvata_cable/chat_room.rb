4fddfabfb4a85c02d15c1ed0161557aba7f2b371
Frank Lam <ryzingsun11@yahoo.com>
2019-07-29 10:11:22 +0800

    Passing string to class_name option
    * Passing a class messes with eager loading and will be deprecated

--- app/models/larvata_cable/chat_room.rb
+++ app/models/larvata_cable/chat_room.rb
@@ -1,5 +1,5 @@
 module LarvataCable
   class ChatRoom < ApplicationRecord
-    belongs_to :owner, class_name: LarvataCable.user_class
+    belongs_to :owner, class_name: LarvataCable.user_class.to_s
   end
 end
