3c994c1916f06d57b94f75c05ecb37a4249570bb
Frank Lam <ryzingsun11@yahoo.com>
2019-07-31 14:47:18 +0800

    Pass string to class_name option
    * Passing class constant messes with autoloading, will be deprecated

--- app/models/larvata_cable/chatter.rb
+++ app/models/larvata_cable/chatter.rb
@@ -1,6 +1,6 @@
 module LarvataCable
   class Chatter < ApplicationRecord
-    belongs_to :user, class_name: LarvataCable.user_class
+    belongs_to :user, class_name: LarvataCable.user_class.to_s
     belongs_to :chat_room, foreign_key: 'larvata_cable_chat_room_id'
   end
 end
