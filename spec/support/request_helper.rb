fc71f8fb0c31ad45ad98b244d638299636779000
Frank Lam <ryzingsun11@yahoo.com>
2019-08-02 11:50:16 +0800

    Remove redundant request helper methods

--- spec/support/request_helper.rb
+++ spec/support/request_helper.rb
@@ -2,14 +2,6 @@ module RequestHelper
   def body_content
     JSON.parse(response.body)
   end
-
-  def off_day?(date)
-    pick_date(date)["is_off"]
-  end
-
-  def pick_date(date)
-    body_content["data"].flatten.detect { |day| day["date_id"] == date }
-  end
 end
 
 RSpec.configure do |config|
