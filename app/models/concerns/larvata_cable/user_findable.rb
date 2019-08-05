a06b1d4e76ac5bf18553ca0905619b5b7fc731f5
Frank Lam <ryzingsun11@yahoo.com>
2019-08-05 13:37:12 +0800

    Extract UserFindable concern
    * ActionCable channels and ApplicationController are quite different
    * Default logger is different:
    https://devdocs.io/rails~5.2/actioncable/connection/taggedloggerproxy#method-i-add_tags
    * env is directly exposed in channels, via request in controller

---
+++ app/models/concerns/larvata_cable/user_findable.rb
@@ -0,0 +1,41 @@
+module LarvataCable
+  module UserFindable
+    extend ActiveSupport::Concern
+
+    included do
+      def find_verified_user(&block)
+        if @user = request.env['warden']&.user || (block.call if block_given?)
+          @user
+        elsif decoded_token
+          LarvataCable.user_class.find(decoded_token['user_id'])
+        else
+          nil
+        end
+      end
+
+      def decoded_token
+        @token ||= LarvataCable::JWTWrapper.decode(request_header_token)
+      rescue JWT::DecodeError => e
+        lawger('JWT Decode Error', "#{e.message}: #{request_header_token.inspect}")
+        false
+      end
+
+      def request_header_token
+        @raw_token = if request.headers['Authorization']&.match(/Bearer /)
+                       request.headers['Authorization'].split[1]
+                     end
+      end
+
+      def lawger(tag, message)
+        if logger.respond_to?(:add_tags)
+          logger.add_tags tag, message
+        else
+          logger.error "[#{tag}] #{message}"
+        end
+      end
+    end
+
+    class_methods do
+    end
+  end
+end
