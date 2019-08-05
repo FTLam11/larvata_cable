b19e31f26bfc125adef1295fd5fc913d3a79829d
Frank Lam <ryzingsun11@yahoo.com>
2019-08-05 16:57:38 +0800

    Remove unused stubs from subscription rejection example

--- spec/channel_spec.rb
+++ spec/channel_spec.rb
@@ -2,9 +2,7 @@ require 'rails_helper'
 
 RSpec.describe ApplicationCable::Channel, type: :channel do
   context 'with no chat room id' do
-    it 'rejects the connection' do
-      user = instance_double(User, id: 322)
-      stub_connection current_user: user
+    it 'rejects the subscription' do
       stub_connection set_chat_room: nil
 
       subscribe
