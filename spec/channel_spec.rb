40e2259b273c382d624171160528de2b453adc4d
Frank Lam <ryzingsun11@yahoo.com>
2019-08-05 17:15:29 +0800

    Test channel subscription with a chat room id

--- spec/channel_spec.rb
+++ spec/channel_spec.rb
@@ -3,11 +3,23 @@ require 'rails_helper'
 RSpec.describe ApplicationCable::Channel, type: :channel do
   context 'with no chat room id' do
     it 'rejects the subscription' do
-      stub_connection set_chat_room: nil
+      stub_connection find_chat_room: nil
 
       subscribe
 
       expect(subscription).to be_rejected
+    end
+  end
+
+  context 'with a chat room id' do
+    it 'subscribes to a chat room stream' do
+      chat_room = instance_double(LarvataCable::ChatRoom, id: 322)
+      stub_connection find_chat_room: chat_room
+
+      subscribe(room_id: 322)
+
+      expect(subscription).to be_confirmed
+      expect(subscription).to have_stream_for(chat_room)
     end
   end
 end
--- spec/dummy/app/channels/application_cable/channel.rb
+++ spec/dummy/app/channels/application_cable/channel.rb
@@ -1,14 +1,17 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
     def subscribed
-      @chat_room = set_chat_room
-      reject unless @chat_room
+      if @chat_room = find_chat_room
+        stream_for @chat_room
+      else
+        reject
+      end
     end
 
     private
 
-    def set_chat_room
-      current_user.chat_rooms.where(id: params[:chat_room_id]).first
+    def find_chat_room
+      @chat_room = current_user.chat_rooms.where(id: params[:chat_room_id]).first
     end
   end
 end
