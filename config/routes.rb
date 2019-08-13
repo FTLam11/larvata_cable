e0752aae05579481752aaaf8e11b1c46dea95b38
Frank Lam <ryzingsun11@yahoo.com>
2019-08-13 10:05:09 +0800

    Add route for sidekiq dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 LarvataCable::Engine.routes.draw do
   post 'login', defaults: { format: :json }, to: 'auth#create'
   delete 'logout', defaults: { format: :json }, to: 'auth#destroy'
+
+  if Rails.env.development?
+    require 'sidekiq/web'
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
