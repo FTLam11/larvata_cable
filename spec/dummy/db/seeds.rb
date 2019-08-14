587a2fa13d350ab110ec32e019b55a54fce38579
Frank Lam <ryzingsun11@yahoo.com>
2019-08-14 11:40:24 +0800

    Add password to User seeds and factory

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -1,8 +1,8 @@
 puts "Seed start"
 
-falcon = User.find_or_create_by(account: 'Falcon')
+falcon = User.find_or_create_by(account: 'Falcon', password: '12345678')
 melee = falcon.chat_rooms.create(name: 'Melee', owner: falcon)
-falco = User.find_or_create_by(account: 'Falco')
+falco = User.find_or_create_by(account: 'Falco', password: '12345678')
 melee.members << falco
 melee.messages.create(sender: falcon, body: 'FALCON PAWNCH')
 melee.messages.create(sender: falco, body: 'AWAAAAAAAAA')
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     account { "larvata_acct" }
     auth_token { nil }
+    password { '12345678' }
   end
 end
