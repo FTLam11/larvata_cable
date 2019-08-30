165b2874b5da686600f66814fc7d79d5cfaa3a64
Frank Lam <ryzingsun11@yahoo.com>
2019-08-30 10:29:52 +0800

    Delete host app User resource

--- spec/dummy/app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  devise :database_authenticatable
-
-  validates_presence_of :account
-  validates_uniqueness_of :account
-end
--- spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :user do
-    account { "larvata_acct" }
-    auth_token { nil }
-    password { '12345678' }
-  end
-end
