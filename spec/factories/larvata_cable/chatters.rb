6c5f3b5f1b38e9c158675d32978dae26e72e48a1
Frank Lam <ryzingsun11@yahoo.com>
2019-08-30 15:16:35 +0800

    Use sequence for generating unique fields in factories

--- spec/factories/larvata_cable/chatters.rb
+++ spec/factories/larvata_cable/chatters.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :larvata_cable_chatter, class: 'LarvataCable::Chatter', aliases: [:chatter] do
-    user { nil }
-    chat_room { nil }
+    association :user
+    association :chat_room
     last_read_at { "2019-07-26 12:13:03" }
   end
 end
--- spec/factories/larvata_cable/tenants.rb
+++ spec/factories/larvata_cable/tenants.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
+  sequence(:tenant_name) { |n| "tenant#{n}" }
+
   factory :larvata_cable_tenant, class: 'LarvataCable::Tenant', aliases: [:tenant] do
     application_id { nil }
-    name { "Tenant" }
+    name { generate(:tenant_name) }
   end
 end
