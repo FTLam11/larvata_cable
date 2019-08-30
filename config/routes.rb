a70772bac0f710d8550fad2efc047fd500976fd1
Frank Lam <ryzingsun11@yahoo.com>
2019-08-30 10:14:51 +0800

    Change authentication route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 LarvataCable::Engine.routes.draw do
-  post 'login', defaults: { format: :json }, to: 'auth#create'
-  delete 'logout', defaults: { format: :json }, to: 'auth#destroy'
+  post 'auth', defaults: { format: :json }, to: 'auth#create'
 
+  # TODO enable authentication for production sidekiq dashboard
   if Rails.env.development?
     require 'sidekiq/web'
     mount Sidekiq::Web => '/sidekiq'
