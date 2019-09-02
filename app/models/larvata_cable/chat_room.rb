c77c577a757096d9dffdca56f18570d8e14b8609
Frank Lam <ryzingsun11@yahoo.com>
2019-09-02 16:05:14 +0800

    Provide custom as_json definition for ChatRoom

--- app/models/larvata_cable/chat_room.rb
+++ app/models/larvata_cable/chat_room.rb
@@ -6,7 +6,16 @@ module LarvataCable
     has_many :members, through: :chatters, source: 'user'
     has_many :messages, -> { order(:created_at) }, foreign_key: 'larvata_cable_chat_room_id'
 
+    # TODO is name required? examine use cases
+
     after_create :add_owner_to_members
+
+    def as_json(*)
+      super(only: [:id, :name]).tap do |hash|
+        hash[:owner_id] = owner.host_user_id
+        hash[:app_id] = tenant_id
+      end
+    end
 
     private
 
