296d0fa08fa8be7244f6bfa0d94cde59951c4069
Frank Lam <ryzingsun11@yahoo.com>
2019-09-02 11:54:27 +0800

    Refactor AuthWrapper using function composition

--- app/lib/larvata_cable/auth_wrapper.rb
+++ app/lib/larvata_cable/auth_wrapper.rb
@@ -2,21 +2,26 @@ module LarvataCable
   module AuthWrapper
     extend self
 
-    # Upgrade Ruby to use function piping feature
+    FN = {
+      encrypt: ->(data) { LarvataCable.auth_box.encrypt(data) },
+      base64_encode: ->(data) { Base64.encode64(data) },
+      utf8_encode: ->(data) { data.encode('UTF-8', 'ASCII-8BIT') },
+      ascii_encode: ->(data) { data.encode('ASCII-8BIT', 'UTF-8') },
+      base64_decode: ->(data) { Base64.decode64(data) },
+      decrypt: ->(data) { LarvataCable.auth_box.decrypt(data) },
+    }.freeze
+
+    GENERATE_TOKEN = FN[:encrypt] >> FN[:base64_encode] >> FN[:utf8_encode]
+    PARSE_TOKEN = FN[:ascii_encode] >> FN[:base64_decode] >> FN[:decrypt]
+
+    private_constant :FN
 
     def generate_token(data)
-      encrypted = LarvataCable.auth_box.encrypt(data.to_json)
-      encoded = Base64.encode64(encrypted)
-      utf_8 = encoded.encode('UTF-8', 'ASCII-8BIT')
-
-      { token: utf_8 }
+      { token: GENERATE_TOKEN.call(data.to_json) }
     end
 
     def parse_token(params)
-      binary = params[:token].encode('ASCII-8BIT', 'UTF-8')
-      decoded = Base64.decode64(binary)
-      decrypted = LarvataCable.auth_box.decrypt(decoded)
-      JSON.parse(decrypted)
+      JSON.parse(PARSE_TOKEN.call(params[:token]))
     end
   end
 end
