e180c4b19616984874674653bfbf4199f6eac0b4
Frank Lam <ryzingsun11@yahoo.com>
2019-09-03 11:04:11 +0800

    Use ! versions in seeds file

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -2,17 +2,17 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190801092527) do
+ActiveRecord::Schema.define(version: 2019_08_01_092527) do
 
-  create_table "larvata_cable_chat_rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "larvata_cable_chat_rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.bigint "owner_id", null: false
     t.string "tenant_id", null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20190801092527) do
     t.index ["tenant_id"], name: "index_larvata_cable_chat_rooms_on_tenant_id"
   end
 
-  create_table "larvata_cable_chatters", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "larvata_cable_chatters", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "larvata_cable_user_id", null: false
     t.bigint "larvata_cable_chat_room_id", null: false
     t.datetime "last_read_at"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 20190801092527) do
     t.index ["larvata_cable_user_id"], name: "index_larvata_cable_chatters_on_larvata_cable_user_id"
   end
 
-  create_table "larvata_cable_messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "larvata_cable_messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "sender_id", null: false
     t.bigint "larvata_cable_chat_room_id", null: false
     t.text "body"
@@ -43,14 +43,14 @@ ActiveRecord::Schema.define(version: 20190801092527) do
     t.index ["sender_id"], name: "index_larvata_cable_messages_on_sender_id"
   end
 
-  create_table "larvata_cable_tenants", primary_key: "application_id", id: :string, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "larvata_cable_tenants", primary_key: "application_id", id: :string, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["application_id"], name: "index_larvata_cable_tenants_on_application_id", unique: true
   end
 
-  create_table "larvata_cable_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci" do |t|
+  create_table "larvata_cable_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "host_user_id", null: false
     t.string "tenant_id", null: false
     t.datetime "created_at", null: false
