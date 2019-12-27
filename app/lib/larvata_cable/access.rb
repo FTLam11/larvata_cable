b2a5a43f5d4e46046907745289720a729b4b53e1
Frank Lam <ryzingsun11@yahoo.com>
2019-12-27 11:41:50 +0800

    Change Access.encode to private

--- app/lib/larvata_cable/access.rb
+++ app/lib/larvata_cable/access.rb
@@ -4,10 +4,6 @@ module LarvataCable
   module Access
     extend self
 
-    def encode(payload)
-      JWT.encode(payload, LarvataCable.signing_key, 'HS256')
-    end
-
     def decode(token)
       JWT.decode(token, LarvataCable.signing_key, true, exp_leeway: LarvataCable.leeway_claim, algorithm: 'HS256').first
     end
@@ -16,6 +12,12 @@ module LarvataCable
       expires_at = Time.zone.now.to_i + LarvataCable.exp_claim_time
 
       encode(sub: user.id, aud: user.tenant_id, exp: expires_at)
+    end
+
+    private
+
+    def encode(payload)
+      JWT.encode(payload, LarvataCable.signing_key, 'HS256')
     end
   end
 end
